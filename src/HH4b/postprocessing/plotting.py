d28d3908fd43a59f12381b3736b739881624525f
Cristina <criss.ms7@gmail.com>
2023-10-05 09:36:36 -0500

    add postprocessing example

---
+++ src/HH4b/postprocessing/plotting.py
@@ -0,0 +1,214 @@
+from hist.intervals import ratio_uncertainty
+
+import matplotlib.pyplot as plt
+import mplhep as hep
+import numpy as np
+
+plt.style.use(hep.style.CMS)
+hep.style.use("CMS")
+
+import matplotlib.ticker as mticker
+formatter = mticker.ScalarFormatter(useMathText=True)
+formatter.set_powerlimits((-3, 3))
+
+import matplotlib as mpl
+mpl.rcParams["font.size"] = 20
+mpl.rcParams["lines.linewidth"] = 2
+mpl.rcParams["grid.color"] = "#CCCCCC"
+mpl.rcParams["grid.linewidth"] = 0.5
+mpl.rcParams["figure.dpi"] = 400
+mpl.rcParams["figure.edgecolor"] = "none"
+
+import os
+
+errps = {
+    "hatch": "////",
+    "facecolor": "none",
+    "lw": 0,
+    "color": "k",
+    "edgecolor": (0, 0, 0, 0.5),
+    "linewidth": 0,
+    "alpha": 0.4,
+}
+data_err_opts = {
+    "linestyle": "none",
+    "marker": ".",
+    "markersize": 10.0,
+    "elinewidth": 1,
+}
+
+color_by_sample = {
+    "gghtobb": "aquamarine",
+    "vbfhtobb": "teal",
+    "tthtobb": "cadetblue",
+    "vhtobb": "tab:cyan",
+    "ttbar": "royalblue",
+    "qcd": "yellow",
+    "diboson": "orchid",
+    "vjets": "tab:green",
+}
+
+label_by_sample = {
+    "gghtobb": "ggH(bb)",
+    "vbfhtobb": "VBFH(bb)",
+    "tthtobb": "ttH(bb)",
+    "vhtobb": "VH(bb)",
+    "qcd": "Multijet",
+    "diboson": "VV",
+    "ttbar": r"$t\bar{t}$+jets",
+    "vjets": r"W/Z$(qq)$",
+}
+
+def plot_hists(
+    year,
+    hists,
+    vars_to_plot,
+):
+    # TODO: add data as boolean
+
+    # TODO: get as input (fb)
+    luminosity = 20.66
+
+    for var in vars_to_plot:
+        if var not in hists.keys():
+            print(f"{var} not stored in hists")
+            continue
+
+        print(f"Will plot {var} histogram")
+        h = hists[var]
+
+        samples = [h.axes[0].value(i) for i in range(len(h.axes[0].edges))]
+        
+        data = h[{"Sample": "data"}]
+
+        signals = ["hh4b"]
+        signal_labels = [label for label in samples if label in signals]
+        bkg_labels = [
+            label
+            for label in samples
+            if (label and label not in signal_labels and (label not in ["data"]))
+        ]
+        bkg = [h[{"Sample": label}] for label in bkg_labels]
+
+        fig, (ax, rax) = plt.subplots(
+            nrows=2,
+            ncols=1,
+            figsize=(8, 8),
+            gridspec_kw={"height_ratios": (4, 1), "hspace": 0.07},
+            sharex=True,
+        )
+
+        # plot data
+        hep.histplot(
+            data,
+            ax=ax,
+            histtype="errorbar",
+            color="k",
+            capsize=4,
+            yerr=True,
+            label="Data",
+            **data_err_opts,
+        )
+
+        # plot bkg
+        hep.histplot(
+            bkg,
+            ax=ax,
+            stack=True,
+            sort="yield",
+            edgecolor="black",
+            linewidth=1,
+            histtype="fill",
+            label=[label_by_sample[bkg_label] for bkg_label in bkg_labels],
+            color=[color_by_sample[bkg_label] for bkg_label in bkg_labels],
+        )
+
+        # sum all the background
+        tot = bkg[0].copy()
+        for i, b in enumerate(bkg):
+            if i > 0:
+                tot = tot + b
+
+        tot_val = tot.values()
+        tot_val_zero_mask = tot_val == 0
+        tot_val[tot_val_zero_mask] = 1
+
+        tot_err = np.sqrt(tot_val)
+        tot_err[tot_val_zero_mask] = 0
+
+        # plot data/mc ratio
+        data_val = data.values()
+        data_val[tot_val_zero_mask] = 1
+        yerr = ratio_uncertainty(data_val, tot_val, "poisson")
+
+        hep.histplot(
+            data_val / tot_val,
+            tot.axes[0].edges,
+            yerr=yerr,
+            ax=rax,
+            histtype="errorbar",
+            color="k",
+            capsize=4,
+        )
+
+        # plot bkg uncertainty
+        ax.stairs(
+            values=tot.values() + tot_err,
+            baseline=tot.values() - tot_err,
+            edges=tot.axes[0].edges,
+            **errps,
+            label="Stat. unc.",
+        )
+
+        ax.set_ylabel("Events")
+        ax.set_xlabel("")
+        rax.set_xlabel(
+            f"{h.axes[-1].label}"
+        )  # assumes the variable to be plotted is at the last axis
+        rax.set_ylabel("Data/MC", fontsize=20)
+        hep.cms.lumitext(
+            "%.1f " % luminosity + r"fb$^{-1}$ (13 TeV)", ax=ax, fontsize=20
+        )
+        hep.cms.text("Work in Progress", ax=ax, fontsize=15)
+
+        # add legend
+        handles, labels = ax.get_legend_handles_labels()
+
+        # get total yield of backgrounds per label
+        first_key = list(hists.keys())[0]
+        # (sort by yield after pre-sel)
+        order_dic = {}
+        for bkg_label in bkg_labels:
+            bkg_yield = hists[first_key][{"Sample": bkg_label}].sum().value
+            order_dic[label_by_sample[bkg_label]] = bkg_yield
+        print(order_dic)
+            
+        summ = [order_dic[label] for label in labels[: len(bkg_labels)]]
+        
+        # get indices of labels arranged by yield
+        order = []
+        for i in range(len(summ)):
+            order.append(np.argmax(np.array(summ)))
+            summ[np.argmax(np.array(summ))] = -100
+            
+        legend_handles = [handles[-1]] + [handles[i] for i in order] + handles[len(bkg) : -1]
+        legend_labels = [labels[-1]] + [labels[i] for i in order] + labels[len(bkg) : -1]
+        
+        ax.legend(
+            [legend_handles[idx] for idx in range(len(legend_handles))],
+            [legend_labels[idx] for idx in range(len(legend_labels))],
+            bbox_to_anchor=(1.05, 1),
+            loc="upper left",
+            # title=
+        )
+
+        logy = True
+        if logy:
+            ax.set_yscale("log")
+            ax.set_ylim(1e-1)
+            
+        outpath = "plots"
+        if not os.path.exists(outpath):
+            os.makedirs(outpath)
+
+        plt.savefig(f"{outpath}/{var}.pdf", bbox_inches="tight")
---
+++ src/HH4b/postprocessing/postprocessing.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python3
+
+import click
+
+import utils
+import plotting
+
+import logging
+import sys
+
+
+# define ShapeVar (label and bins for a given variable)
+from utils import ShapeVar
+
+
+var_to_shapevar = {
+    # var must match key in events dictionary (i.e. as saved in parquet file)
+    "DijetMass": ShapeVar(
+        var="DijetMass", label=r"$m^{jj}$ (GeV)", bins=[30, 600, 4000]
+    ),
+    "ak8FatJetPt0": ShapeVar(
+        var="ak8FatJetPt0", label=r"$p_T^0$ (GeV)", bins=[30, 300, 1500], significance_dir="right"
+    ),
+    "ak8FatJetPt1": ShapeVar(
+        var="ak8FatJetPt1", label=r"$p_T^1$ (GeV)", bins=[30, 300, 1500], significance_dir="right"
+    ),
+    "ak8FatJetPNetMass0": ShapeVar(
+        var="ak8FatJetPNetMass0", label=r"$m_{reg}^{0}$ (GeV)", bins=[20, 50, 250]
+    ),
+    "ak8FatJetPNetXbb0": ShapeVar(
+        var="ak8FatJetPNetXbb0", label=r"$TX_{bb}^{0}$", bins=[50, 0., 1],
+    ),
+}
+
+
+@click.command()
+@click.option(
+    "--year",
+    "years",
+    required=True,
+    multiple=True,
+    type=click.Choice(["2022", "2022EE", "2023", "2018"], case_sensitive=False),
+    help="year",
+)
+def postprocess(years):
+    # TODO: set this as a yaml file
+    dirs = {
+        "/eos/uscms/store/user/cmantill/bbbb/skimmer/Oct2/": {
+            "qcd": [
+                "QCD_PT-120to170",
+                "QCD_PT-170to300",
+                "QCD_PT-470to600",
+                "QCD_PT-600to800",
+                "QCD_PT-800to1000",
+                "QCD_PT-1000to1400",
+                "QCD_PT-1400to1800",
+                "QCD_PT-1800to2400",
+                "QCD_PT-2400to3200",
+                "QCD_PT-3200",
+            ],
+            "data": [
+                "Run2022F",
+                "Run2022G",
+            ],
+            "ttbar": [
+                "TTtoLNu2Q",
+                "TTto4Q",
+                "TTto2L2Nu",
+            ],
+            "gghtobb": [
+                "GluGluHto2B_PT-200_M-125",
+            ],
+            "vbfhtobb": [
+                "VBFHto2B_M-125_dipoleRecoilOn",
+            ],
+            "vhtobb": [
+                "WplusH_Hto2B_Wto2Q_M-125",
+                "WplusH_Hto2B_WtoLNu_M-125",
+                "WminusH_Hto2B_Wto2Q_M-125",
+                "WminusH_Hto2B_WtoLNu_M-125",
+                "ZH_Hto2B_Zto2Q_M-125",
+                "ggZH_Hto2B_Zto2Q_M-125",
+                "ggZH_Hto2B_Zto2L_M-125",
+                "ggZH_Hto2B_Zto2Nu_M-125",
+            ],
+            "tthtobb": [
+                "ttHto2B_M-125",
+            ],
+            "diboson": [
+                "ZZ",
+                "WW",
+                "WZ",
+            ],
+            "vjets": [
+                "Wto2Q-3Jets_HT-200to400",
+                "Wto2Q-3Jets_HT-400to600",
+                "Wto2Q-3Jets_HT-600to800",
+                "Wto2Q-3Jets_HT-800",
+                "Zto2Q-4Jets_HT-200to400",
+                "Zto2Q-4Jets_HT-400to600",
+                "Zto2Q-4Jets_HT-600to800",
+                "Zto2Q-4Jets_HT-800",
+            ],
+        }
+    }
+    samples_to_fill = [
+        "data",
+        "qcd",
+    ]
+    vars_to_plot = [
+        "ak8FatJetPt0",
+        "ak8FatJetPt1",
+        "DijetMass",
+        "ak8FatJetPNetXbb0",
+    ]
+
+    # weight to apply to histograms
+    weight_key = ["finalWeight"]
+
+    # filters are sequences of strings that can be used to place a selection
+    # e.g. https://github.com/rkansal47/HHbbVV/blob/main/src/HHbbVV/postprocessing/postprocessing.py#L80
+    filters = [
+        [
+            #[
+            #    ("('HLT_AK8PFJet250_SoftDropMass40_PFAK8ParticleNetBB0p35', '0')", "==", 1),
+            #    ("('HLT_AK8PFJet425_SoftDropMass40', '0')", "==", 1),
+            #],
+            #("('HLT_AK8PFJet425_SoftDropMass40', '0')", "==", 1),
+            ("('ak8FatJetPt', '0')", ">=", 300),
+            ("('ak8FatJetPt', '1')", ">=", 250),
+            ("('ak8FatJetMsd', '0')", ">=", 60),
+            ("('ak8FatJetMsd', '1')", ">=", 60),
+            # ("('ak8FatJetPNetXbb', '0')", ">=", 0.8),
+            # ("('ak8FatJetPNetXbb', '1')", ">=", 0.8),
+        ],
+    ]
+
+    # columns to load
+    load_columns = [
+        ("weight", 1),
+        ("DijetMass", 1),
+        ("ak8FatJetPt", 2),
+        ("ak8FatJetPNetXbb", 2),
+        #"single_weight_trigsf_2jet"
+        # ("ak8FatJetPNetMass", 2),
+    ]
+    # reformat into ("column name", "idx") format for reading multiindex columns
+    columns = []
+    for key, num_columns in load_columns:
+        for i in range(num_columns):
+            columns.append(f"('{key}', '{i}')")
+
+    for year in years:
+        # load all samples, apply filters if needed
+        events_dict = {}
+        for input_dir, samples in dirs.items():
+            events_dict = {
+                **events_dict,
+                **utils.load_samples(input_dir, samples, year, filters, columns)
+            }
+
+        samples_loaded = list(events_dict.keys())
+        keys_loaded = list(events_dict[samples_loaded[0]].keys())
+        # print(f"Keys in events_dict {keys_loaded}")
+
+        # make a histogram
+        hists = {}
+        for var in vars_to_plot:
+            shape_var = var_to_shapevar[var]
+            if shape_var.var not in hists:
+                hists[shape_var.var] = utils.singleVarHist(
+                    events_dict,
+                    shape_var,
+                    weight_key=weight_key,
+                    selection=None,
+                )
+
+        # make a stacked plot
+        plotting.plot_hists(
+            year,
+            hists,
+            vars_to_plot,
+        )
+
+
+if __name__ == "__main__":
+    sys.exit(postprocess())
