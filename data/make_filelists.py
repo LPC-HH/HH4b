b8e69ee4810638a69c2fda65ff083e166aaeb553
Cristina <criss.ms7@gmail.com>
2023-10-05 07:23:33 -0500

    add pass on rucio

--- data/make_filelists.py
+++ data/make_filelists.py
@@ -1,8 +1,10 @@
 import json
 import subprocess
 from dbs.apis.dbsClient import DbsApi
-
 dbs = DbsApi("https://cmsweb.cern.ch/dbs/prod/global/DBSReader")
+
+import os
+os.environ["RUCIO_HOME"] = "/cvmfs/cms.cern.ch/rucio/x86_64/rhel7/py3/current"
 
 qcd_bins = [
     # "0to80",
@@ -483,7 +485,7 @@ def eos_rec_search(startdir, suffix, dirs):
     donedirs = [[] for d in dirlook]
     for di, d in enumerate(dirlook):
         if d.endswith(suffix):
-            donedirs[di].append(startdir + "/" + d)
+            donedirs[di].append(f"root://cmsxrootd-site.fnal.gov/{startdir}/{d}")
         elif d == "log":
             continue
         else:
@@ -494,7 +496,7 @@ def eos_rec_search(startdir, suffix, dirs):
     return dirs + donedir
 
 
-for version in ["v9", "v10", "v11", "v11_private"]:
+for version in ["v9"]: # , "v10", "v11", "v11_private"]:
     datasets = globals()[f"get_{version}"]()
     index = datasets.copy()
     for year, ydict in datasets.items():
@@ -503,7 +505,25 @@ for version in ["v9", "v10", "v11", "v11_private"]:
                 if "private" in version:
                     files = eos_rec_search(dataset, ".root", [])
                 else:
-                    files = [ifile["logical_file_name"] for ifile in dbs.listFiles(dataset=dataset)]
+                    from rucio import get_proxy_path
+                    import requests
+                    proxy = get_proxy_path()
+                    link = f"https://cmsweb.cern.ch:8443/dbs/prod/global/DBSReader/files?dataset={dataset}&detail=True"
+                    r = requests.get(
+                        link,
+                        cert=proxy,
+                        verify=False,
+                    )
+                    filesjson = r.json()
+                    for fj in filesjson:
+                        if fj["is_file_valid"] == 0:
+                            print(f"ERROR: File not valid on DAS: {f['name']}")
+                        else:
+                            print(fj['logical_file_name'])
+                            #self.fileslist_redirector.append(fj['logical_file_name'])
+                            #self.metadata["nevents"] += fj['event_count']
+                            #self.metadata["size"] += fj['file_size']
+                    # files = [ifile["logical_file_name"] for ifile in dbs.listFiles(dataset=dataset)]
                 index[year][sample][sname] = files
 
     with open(f"nanoindex_{version}.json", "w") as f:
