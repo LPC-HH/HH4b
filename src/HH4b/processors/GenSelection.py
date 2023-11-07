c1c3e1573460310d9a7196eef038a54231f47c56
Cristina <criss.ms7@gmail.com>
2023-11-07 15:36:00 -0600

    add h gen selection

--- src/HH4b/processors/GenSelection.py
+++ src/HH4b/processors/GenSelection.py
@@ -128,7 +128,7 @@ def gen_selection_HHbbbb(
     # print("drhiggs", fatjets.metric_table(higgs) < 0.8)
     # print(fatjets["HiggsMatchIndex"])
 
-    num_fatjets = 3
+    num_fatjets = 2
     ak8FatJetVars = {
         f"ak8FatJet{var}": pad_val(fatjets[var], num_fatjets, axis=1)
         for var in [
@@ -142,3 +142,52 @@ def gen_selection_HHbbbb(
     }
 
     return {**GenHiggsVars, **GenbVars, **ak4JetVars, **ak8FatJetVars}
+
+
+def gen_selection_Hbb(
+    events: NanoEventsArray,
+    jets: JetArray,
+    fatjets: FatJetArray,
+    selection: PackedSelection,
+    cutflow: dict,
+    signGenWeights: ak.Array,
+    skim_vars: dict,
+):
+    """Gets H, bb, 4-vectors + Higgs children information"""
+
+    # finding the two gen higgs
+    higgs = events.GenPart[
+        (abs(events.GenPart.pdgId) == HIGGS_PDGID) * events.GenPart.hasFlags(GEN_FLAGS)
+    ]
+    higgs_children = higgs.children
+
+    GenHiggsVars = {f"GenHiggs{key}": higgs[var].to_numpy() for (var, key) in skim_vars.items()}
+    GenHiggsVars["GenHiggsChildren"] = abs(higgs_children.pdgId[:, :, 0]).to_numpy()
+
+    is_bb = (abs(higgs_children.pdgId) == b_PDGID)
+    bs = ak.flatten(higgs_children[is_bb], axis=2)
+    GenbVars = {f"Genb{key}": pad_val(bs[var], 4, axis=1) for (var, key) in skim_vars.items()}
+
+    # match fatjets to bb
+    num_b_matched = ak.sum(fatjets.metric_table(bs_unflat) < 0.8, axis=2)
+    matched_to_higgs = fatjets.metric_table(higgs) < 0.8
+
+    fatjets["HiggsMatch"] = is_fatjet_matched
+    fatjets["HiggsMatchIndex"] = ak.mask(
+        ak.argmin(fatjets.metric_table(higgs), axis=2), fatjets["HiggsMatch"] == 1
+    )
+    fatjets["NumBMatchedH1"] = ak.sum(fatjets.metric_table(b_h1) < 0.8, axis=2)
+    fatjets["NumBMatchedH2"] = ak.sum(fatjets.metric_table(b_h2) < 0.8, axis=2)
+
+    num_fatjets = 2
+    ak8FatJetVars = {
+        f"ak8FatJet{var}": pad_val(fatjets[var], num_fatjets, axis=1)
+        for var in [
+            "HiggsMatch",
+            "HiggsMatchIndex",
+            "NumBMatchedH1",
+            "NumBMatchedH2",
+        ]
+    }
+
+    return {**GenHiggsVars, **GenbVars}
