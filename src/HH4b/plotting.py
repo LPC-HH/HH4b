71eddbc431ac314922bea439b324cbf3c8766e20
Daniel Diaz <ddiaz006@ucr.edu>
2024-02-20 13:58:02 -0600

    fix precommit craziness

--- src/HH4b/plotting.py
+++ src/HH4b/plotting.py
@@ -593,10 +593,9 @@ def ratioHistPlot(
     labels = labels[-1:] + labels[len(bg_keys) : -1] + labels[: len(bg_keys)][::-1]
     ax.legend(handles, labels, bbox_to_anchor=(1.03, 1), loc="upper left")
 
-    if ylim_low is not None:
-        y_lowlim = ylim_low
-    else:
-        y_lowlim = 0 if not log else 1e-3
+    
+    y_lowlim = ylim_low if ylim_low is not None else 0 if not log else 0.001
+
     if ylim is not None:
         ax.set_ylim([y_lowlim, ylim])
     else:
