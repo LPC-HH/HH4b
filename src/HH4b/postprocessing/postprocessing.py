97d6c2fb0ec9c7764925cbd8490519deda684c1e
rkansal47 <raghavk1997@yahoo.co.in>
2024-03-28 19:05:26 -0400

    update get templates

--- src/HH4b/postprocessing/postprocessing.py
+++ src/HH4b/postprocessing/postprocessing.py
@@ -346,30 +346,12 @@ def get_templates(
                     if sample in wsyst.samples and year in wsyst.years:
                         # print(wshift)
                         for skey, shift in [("Down", "down"), ("Up", "up")]:
-                            if "QCDscale" in wshift:
-                                # QCDscale7pt/QCDscale4
-                                # https://github.com/LPC-HH/HHLooper/blob/master/python/prepare_card_SR_final.py#L263-L288
-                                sweight = (
-                                    weight
-                                    * (
-                                        events[f"weight_QCDscale7pt{skey}"][0]
-                                        / events["weight_QCDscale4"]
-                                    )
-                                    .to_numpy()
-                                    .squeeze()
-                                )
-                            else:
-                                # reweight based on diff between up/down and nominal weights
-                                sweight = (
-                                    weight
-                                    * (
-                                        events[f"weight_{wshift}{skey}"][0]
-                                        / events["weight_nonorm"]
-                                    )
-                                    .to_numpy()
-                                    .squeeze()
-                                )
-                            h.fill(Sample=f"{sample}_{wshift}_{shift}", **fill_data, weight=sweight)
+                            # reweight based on diff between up/down and nominal weights
+                            h.fill(
+                                Sample=f"{sample}_{wshift}_{shift}",
+                                **fill_data,
+                                weight=events[f"weight_{wshift}{skey}"].to_numpy().squeeze(),
+                            )
 
         if pass_region:
             # blind signal mass windows in pass region in data
