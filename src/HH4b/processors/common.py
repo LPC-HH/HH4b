c5a1d8589833f672cd2e1912e219891d40b435cf
Cristina <criss.ms7@gmail.com>
2024-03-31 12:17:03 -0500

    update lumi

--- src/HH4b/processors/common.py
+++ src/HH4b/processors/common.py
@@ -5,11 +5,10 @@ from __future__ import annotations
 
 LUMI = {  # in pb^-1
     "2018": 59830.0,
-    # 5.0707 + 3.0063
-    "2022": 7971.4,
+    "2022": 7971.4,  # 5.0707 + 3.0063
     "2022EE": 26337.0,
-    "2023-pre-BPix": 17981.0,  # runC
-    "2023-BPix": 9516.0,  # runD
+    "2023": 17981.0,  # runC
+    "2023BPix": 9516.0,  # runD
 }
 
 # TODO: split over sources of JECs
