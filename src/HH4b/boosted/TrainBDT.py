f6f74b083ce2cf07ba8ac08f8a27575c77330e5a
Cristina Mantilla Suarez <cristina.ana.mantilla.suarez@cern.ch>
2024-04-18 13:26:00 -0500

    first trainings

--- src/HH4b/boosted/TrainBDT.py
+++ src/HH4b/boosted/TrainBDT.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-
 import argparse
 import importlib
 import pickle
@@ -31,21 +30,31 @@ plt.rcParams["grid.linewidth"] = 0.5
 plt.rcParams["figure.edgecolor"] = "none"
 plt.style.use(hep.style.CMS)
 
+training_keys = ["hh4b", "qcd", "ttbar"]
 
-def load_data(data_path: str, year: str):
+def load_data(data_path: str, year: str, legacy: bool):
     """
     Load samples
     """
 
-    # TODO: integrate all years
-
     samples = {
-        # "2022": {
+        "2022": {
         #    "hh4b": "GluGlutoHHto4B_kl-1p00_kt-1p00_c2-0p00_TuneCP5_13p6TeV_Private",
-        # },
+            "qcd": [
+                "QCD_HT-400to600",
+                "QCD_HT-600to800",
+                "QCD_HT-800to1000",
+                "QCD_HT-1000to1200",
+                "QCD_HT-1200to1500",
+                "QCD_HT-1500to2000",
+                "QCD_HT-2000",
+            ],
+            "ttbar": [
+                "TTto4Q",
+            ],
+        },
         "2022EE": {
             "qcd": [
-                "QCD_HT-200to400",
                 "QCD_HT-400to600",
                 "QCD_HT-600to800",
                 "QCD_HT-800to1000",
@@ -79,21 +88,58 @@ def load_data(data_path: str, year: str):
                 "Zto2Q-4Jets_HT-600to800",
                 "Zto2Q-4Jets_HT-800",
             ],
-        }
+        },
+        "2023": {
+            "qcd": [
+                "QCD_HT-400to600",
+                "QCD_HT-600to800",
+                "QCD_HT-800to1000",
+                "QCD_HT-1000to1200",
+                "QCD_HT-1200to1500",
+                "QCD_HT-1500to2000",
+                "QCD_HT-2000",
+            ],
+            "ttbar": [
+                "TTto4Q",
+            ],
+        },
+        "2023BPix": {
+            "qcd": [
+                "QCD_HT-400to600",
+                "QCD_HT-600to800",
+                "QCD_HT-800to1000",
+                "QCD_HT-1000to1200",
+                "QCD_HT-1200to1500",
+                "QCD_HT-1500to2000",
+                "QCD_HT-2000",
+            ],
+            "ttbar": [
+                "TTto4Q",
+            ],
+        },
     }
 
     dirs = {data_path: samples}
 
-    filters = [
-        [
-            ("('bbFatJetPt', '0')", ">=", 300),
-            ("('bbFatJetPt', '1')", ">=", 300),
-            ("('bbFatJetMsd', '0')", "<=", 250),
-            ("('bbFatJetMsd', '1')", "<=", 250),
-            ("('bbFatJetMsd', '0')", ">=", 30),
-            ("('bbFatJetMsd', '1')", ">=", 30),
-        ],
-    ]
+    if legacy:
+        filters = [
+            [
+                ("('bbFatJetPt', '0')", ">=", 300),
+                ("('bbFatJetPt', '1')", ">=", 300),
+            ]
+        ]
+    else:
+        filters = [
+            [
+                ("('bbFatJetPt', '0')", ">=", 300),
+                ("('bbFatJetPt', '1')", ">=", 300),
+                ("('bbFatJetMsd', '0')", "<=", 250),
+                ("('bbFatJetMsd', '1')", "<=", 250),
+                ("('bbFatJetMsd', '0')", ">=", 30),
+                ("('bbFatJetMsd', '1')", ">=", 30),
+            ],
+        ]
+        
     load_columns = [
         ("weight", 1),
         ("event", 1),
@@ -109,6 +155,11 @@ def load_data(data_path: str, year: str):
         ("bbFatJetPNetQCD1HF", 2),
         ("bbFatJetPNetQCD2HF", 2),
     ]
+    if legacy:
+        load_columns += [
+            ("bbFatJetPNetMassLegacy", 2),
+            ("bbFatJetPNetXbbLegacy", 2)
+        ]
 
     events_dict = {}
     for input_dir, samples in dirs.items():
@@ -124,13 +175,14 @@ def load_data(data_path: str, year: str):
             ),
         }
 
+    print(events_dict.keys())
+    
     return events_dict
 
 
 def preprocess_data(
     events_dict: dict, config_name: str, test_size: float, seed: int, multiclass: bool
 ):
-    training_keys = ["hh4b", "qcd", "ttbar"]
 
     # dataframe function
     make_bdt_dataframe = importlib.import_module(f"{config_name}")
@@ -213,6 +265,22 @@ def preprocess_data(
     )
 
 
+def plot_losses(trained_model: xgb.XGBClassifier, model_dir: Path):
+    evals_result = trained_model.evals_result()
+
+    with (model_dir / "evals_result.txt").open("w") as f:
+        f.write(str(evals_result))
+
+    plt.figure(figsize=(10, 8))
+    for i, label in enumerate(["Train", "Test"]):
+        plt.plot(evals_result[f"validation_{i}"]["logloss"], label=label, linewidth=2)
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.savefig(model_dir / "losses.pdf", bbox_inches="tight")
+    plt.close()
+
+
 def train_model(
     X_train: np.ndarray,
     X_test: np.ndarray,
@@ -225,10 +293,24 @@ def train_model(
 ):
     """Trains BDT. ``classifier_params`` are hyperparameters for the classifier"""
 
+    early_stopping_callback = xgb.callback.EarlyStopping(
+        rounds=5, min_delta=0.0
+    )
+    classifier_params = {**classifier_params, "callbacks": [early_stopping_callback]}
+    
     print("Training model")
     model = xgb.XGBClassifier(**classifier_params)
     print("Training features: ", list(X_train.columns))
 
+    num_hh4b = X_train.loc["hh4b"].shape[0]
+    num_qcd = X_train.loc["qcd"].shape[0]
+    num_tt = X_train.loc["ttbar"].shape[0]
+    print("Number of events training (hh4b,qcd,tt): ", num_hh4b, num_qcd, num_tt)
+    num_hh4b = X_test.loc["hh4b"].shape[0]
+    num_qcd = X_test.loc["qcd"].shape[0]
+    num_tt = X_test.loc["ttbar"].shape[0]
+    print("Number of events testing (hh4b,qcd,tt): ", num_hh4b, num_qcd, num_tt)
+    
     trained_model = model.fit(
         X_train,
         y_train,
@@ -238,6 +320,7 @@ def train_model(
         verbose=True,
     )
     trained_model.save_model(model_dir / "trained_bdt.model")
+    plot_losses(trained_model, model_dir)
 
     # sorting by importance
     importances = model.feature_importances_
@@ -256,18 +339,22 @@ def evaluate_model(
     model: xgb.XGBClassifier,
     model_dir: Path,
     X_test: pd.DataFrame,
-    y_test: pd.DataFrame,
+    y_test: pd.DataFrame, # noqa: ARG001
     yt_test: pd.DataFrame,
     weights_test: np.ndarray,
     # test_size: float, seed: int,
     # year: str,
     multiclass: bool,
+    legacy: bool,
 ):
     """
     1) Makes ROC curves for testing data
     2) Prints Sig efficiency at Bkg efficiency
     """
 
+    pnet_xbb_str = "bbFatJetPNetXbb" if not legacy else "bbFatJetPNetXbbLegacy"
+    pnet_mass_str = "bbFatJetPNetMass" if not legacy else "bbFatJetPNetMassLegacy"
+    
     # make and save ROCs for testing data
     def find_nearest(array, value):
         array = np.asarray(array)
@@ -328,14 +415,15 @@ def evaluate_model(
         test_preds = model.predict_proba(test_bdt_dataframe)
         scores[key] = test_preds[:, 0] if multiclass else test_preds[:, 1]
         weights[key] = test_dataset["finalWeight"]
-        mass_dict[key] = test_dataset["bbFatJetPNetMass"].to_numpy()[:, 1]
+        mass_dict[key] = test_dataset[pnet_mass_str].to_numpy()[:, 1]
         msd_dict[key] = test_dataset["bbFatJetMsd"].to_numpy()[:, 1]
-        xbb_dict[key] = test_dataset["bbFatJetPNetXbb"].to_numpy()[:, 1]
+        xbb_dict[key] = test_dataset[pnet_xbb_str].to_numpy()[:, 1]
+            
     for key in ["vhtobb", "vjets", "ttlep"]:
         preds = model.predict_proba(make_bdt_dataframe.bdt_dataframe(events_dict[key]))
         scores[key] = preds[:, 0] if multiclass else preds[:, 1]
         weights[key] = events_dict[key]["finalWeight"]
-        xbb_dict[key] = events_dict[key]["bbFatJetPNetXbb"].to_numpy()[:, 1]
+        xbb_dict[key] = events_dict[key][pnet_xbb_str].to_numpy()[:, 1]
 
     # save scores and indices for testing dataset
     # TODO: add shifts (e.g. JECs etc)
@@ -343,7 +431,7 @@ def evaluate_model(
     # np.save(f"{model_dir}/inferences/{year}/preds.npy", scores["hh4b"])
     # np.save(f"{model_dir}/inferences/{year}/indices.npy", hh4b_indices)
 
-    print("Scores ", scores)
+    #print("Scores ", scores)
 
     bdt_axis = hist.axis.Regular(40, 0, 1, name="bdt", label=r"BDT")
     cat_axis = hist.axis.StrCategory([], name="cat", growth=True)
@@ -401,7 +489,8 @@ def evaluate_model(
         ax.yaxis.grid(True, which="major")
         fig.tight_layout()
         fig.savefig(model_dir / f"bdt_shape_{h_key}.png")
-
+        plt.close()
+        
     # Plot and save ROC figure
     fig, ax = plt.subplots(1, 1, figsize=(18, 12))
     bkg_colors = {
@@ -486,7 +575,7 @@ def evaluate_model(
     ax.set_ylabel("Background efficiency")
     ax.set_xlim([0.0, 0.7])
     # ax.set_ylim([0, 0.002])
-    ax.set_ylim([0, 0.07])
+    ax.set_ylim([0, 0.08])
     ax.xaxis.grid(True, which="major")
     ax.yaxis.grid(True, which="major")
     ax.legend(
@@ -501,7 +590,8 @@ def evaluate_model(
     )
     fig.tight_layout()
     fig.savefig(model_dir / "roc_weights.png")
-
+    plt.close()
+    
     # look into mass sculpting
     cat_axis = hist.axis.StrCategory([], name="Sample", growth=True)
     cut_axis = hist.axis.StrCategory([], name="Cut", growth=True)
@@ -526,7 +616,7 @@ def evaluate_model(
             h2_mass = mass_dict[key]
             h2_msd = msd_dict[key]
         else:
-            h2_mass = events["bbFatJetPNetMass"].to_numpy()[:, 1]
+            h2_mass = events[pnet_mass_str].to_numpy()[:, 1]
             h2_msd = events["bbFatJetMsd"].to_numpy()[:, 1]
 
         for cut in bdt_cuts:
@@ -552,7 +642,8 @@ def evaluate_model(
             ax.yaxis.grid(True, which="major")
             fig.tight_layout()
             fig.savefig(model_dir / f"{hkey}2_{key}.png")
-
+            plt.close()
+            
     # mass sculpting with Xbb
     hist_h2 = hist.Hist(h2_mass_axis, cut_axis, cat_axis)
     hist_h2_msd = hist.Hist(h2_msd_axis, cut_axis, cat_axis)
@@ -566,9 +657,9 @@ def evaluate_model(
                 h2_msd = msd_dict[key]
                 h2_xbb = xbb_dict[key]
             else:
-                h2_mass = events["bbFatJetPNetMass"].to_numpy()[:, 1]
+                h2_mass = events[pnet_mass_str].to_numpy()[:, 1]
                 h2_msd = events["bbFatJetMsd"].to_numpy()[:, 1]
-                h2_xbb = events["bbFatJetPNetXbb"].to_numpy()[:, 1]
+                h2_xbb = events[pnet_xbb_str].to_numpy()[:, 1]
 
             for cut in bdt_cuts:
                 mask = (scores[key] >= cut) & (h2_xbb > xbb_cut)
@@ -595,10 +686,113 @@ def evaluate_model(
                 ax.yaxis.grid(True, which="major")
                 fig.tight_layout()
                 fig.savefig(model_dir / f"{hkey}2_{key}_xbbcut{xbb_cut}.png")
+                plt.close()
+                
+def plot_train_test(X_train, yt_train, weights_train, X_test, yt_test, weights_test, model, multiclass, model_dir):
+    y_scores_train = model.predict_proba(X_train)
+    y_scores_train = y_scores_train[:, 0] if multiclass else y_scores_train[:, 1]
+    fpr_train, tpr_train, thresholds_train = roc_curve(yt_train, y_scores_train, sample_weight=weights_train)
 
+    y_scores_test = model.predict_proba(X_test)
+    y_scores_test = y_scores_test[:, 0] if multiclass else y_scores_test[:, 1]
+    fpr, tpr, thresholds = roc_curve(yt_test, y_scores_test, sample_weight=weights_test)
 
+    fig, ax = plt.subplots(1, 1, figsize=(18, 12))
+    plot_thresholds = [0.7, 0.9, 0.97]
+    th_colours = ["#9381FF", "#1f78b4", "#a6cee3"]
+    ax.plot(tpr_train, fpr_train, linewidth=2, color="orange", label="Train Dataset")
+    ax.plot(tpr, fpr, linewidth=2, color="orange", label="Test Dataset")
+    ax.set_title("ggF HH4b BDT ROC Curve from Training")
+    ax.set_xlabel("Signal efficiency")
+    ax.set_ylabel("Background efficiency")
+    ax.set_xlim([0.0, 0.7])
+    ax.set_ylim([0, 0.07])
+    ax.xaxis.grid(True, which="major")
+    ax.yaxis.grid(True, which="major")
+    ax.legend(
+        title=r"FatJet p$_T^{(0,1)}$ > 300 GeV"
+        + "\n"
+        + "Xbb$^{0}$>0.8"
+        + "\n"
+        + r"m$_{SD}^{(0,1)}$:[30-250] GeV"
+        + "\n",
+        bbox_to_anchor=(1.03, 1),
+        loc="upper left",
+    )
+    fig.tight_layout()
+    fig.savefig(model_dir / "roc_train_test.png")
+
+    bdt_axis = hist.axis.Regular(40, 0, 1, name="bdt", label=r"BDT")
+    cat_axis = hist.axis.StrCategory([], name="cat", growth=True)
+    h_bdt_weight = hist.Hist(bdt_axis, cat_axis)
+    for key in ["qcd", "ttbar", "hh4b"]:
+        scores = model.predict_proba( X_test.loc[key] )
+        scores = scores[:, 0] if multiclass else scores[:, 1]
+        print(weights_test.loc[key])
+        h_bdt_weight.fill(bdt=scores, cat=key, weight=weights_test.loc[key])
+    for key in ["qcd", "ttbar", "hh4b"]:
+        scores = model.predict_proba( X_train.loc[key] )
+        scores = scores[:, 0] if multiclass else scores[:, 1]
+        h_bdt_weight.fill(bdt=scores, cat=key+"train", weight=weights_train.loc[key])
+
+    colors = {
+        "ttbar": "b",
+        "hh4b": "k",
+        "qcd": "r",
+        "vhtobb": "g",
+        "vjets": "pink",
+        "ttlep": "violet",
+    }
+    legends = {
+        "ttbar": r"$t\bar{t}$ + Jets",
+        "hh4b": "ggHH(4b)",
+        "qcd": "Multijet",
+        "vhtobb": "VH(bb)",
+        "vjets": r"W/Z$(qq)$ + Jets",
+        "ttlep": r"$t\bar{t}$ (Lep.) + Jets",
+    }
+    fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+    for key in ["qcd", "ttbar", "hh4b"]:
+        hep.histplot(
+            h_bdt_weight[{"cat": key}],
+            ax=ax,
+            label=f"{legends[key]}",
+            histtype="step",
+            linewidth=1,
+            color=colors[key],
+            density=True,
+        )
+        hep.histplot(
+            h_bdt_weight[{"cat": key+"train"}],
+            ax=ax,
+            label=f"{legends[key]} Train",
+	    histtype="step",
+            linewidth=1,
+            linestyle="dashed",
+            color=colors[key],
+            density=True,
+        )
+        ax.set_yscale("log")
+        ax.legend(
+            title=r"FatJet p$_T^{(0,1)}$ > 300 GeV"
+            + "\n"
+            + "Xbb$^{0}$>0.8"
+            + "\n"
+            + r"m$_{SD}^{(0,1)}$:[30-250] GeV",
+            bbox_to_anchor=(1.03, 1),
+            loc="upper left",
+        )
+        ax.set_ylabel("Density")
+        ax.set_title("Pre-Selection")
+        ax.xaxis.grid(True, which="major")
+        ax.yaxis.grid(True, which="major")
+    fig.tight_layout()
+    fig.savefig(model_dir / f"bdt_shape_traintest.png")
+        
 def main(args):
-    events_dict = load_data(args.data_path, args.year)
+    #for year in ["2022", "2022EE", "2023", "2023BPix"]:
+    year = "2022EE"
+    events_dict = load_data(args.data_path, year, args.legacy)
 
     (
         X_train,
@@ -607,27 +801,27 @@ def main(args):
         y_test,
         weights_train,
         weights_test,
-        _,
+        yt_train,
         yt_test,
-        _,
+        ev_train,
         ev_test,
     ) = preprocess_data(events_dict, args.config_name, args.test_size, args.seed, args.multiclass)
 
     model_dir = Path(f"./bdt_trainings_run3/{args.model_name}/")
     model_dir.mkdir(exist_ok=True, parents=True)
 
-    (model_dir / "inferences" / args.year).mkdir(exist_ok=True, parents=True)
-    ev_hh4b = ev_test[ev_test.index.get_level_values(0) == "hh4b"]
-    ev_qcd = ev_test[ev_test.index.get_level_values(0) == "qcd"]
-    ev_tt = ev_test[ev_test.index.get_level_values(0) == "ttbar"]
-    np.save(f"{model_dir}/inferences/{args.year}/evt_hh4b.npy", ev_hh4b)
-    np.save(f"{model_dir}/inferences/{args.year}/evt_qcd.npy", ev_qcd)
-    np.save(f"{model_dir}/inferences/{args.year}/evt_ttbar.npy", ev_tt)
+    (model_dir / "inferences" / year).mkdir(exist_ok=True, parents=True)
+    ev_hh4b = ev_test.loc["hh4b"]
+    ev_qcd = ev_test.loc["qcd"]
+    ev_tt = ev_test.loc["ttbar"]
+    np.save(f"{model_dir}/inferences/{year}/evt_hh4b.npy", ev_hh4b)
+    np.save(f"{model_dir}/inferences/{year}/evt_qcd.npy", ev_qcd)
+    np.save(f"{model_dir}/inferences/{year}/evt_ttbar.npy", ev_tt)
 
     classifier_params = {
-        "max_depth": 17,
+        "max_depth": 3,
         "learning_rate": 0.1,
-        "n_estimators": 196,
+        "n_estimators": 1000,
         "verbosity": 2,
         "reg_lambda": 1.0,
     }
@@ -647,18 +841,19 @@ def main(args):
             model_dir,
             **classifier_params,
         )
-
+        plot_train_test(X_train, yt_train, weights_train, X_test, yt_test, weights_test, model, args.multiclass, model_dir)
+        
     evaluate_model(
         args.config_name,
         events_dict,
         model,
         model_dir,
         X_test,
-        # y_test,
+        y_test,
         yt_test,
         weights_test,
-        # args.year,
         args.multiclass,
+        args.legacy,
     )
 
 
@@ -668,12 +863,6 @@ if __name__ == "__main__":
         "--data-path",
         required=True,
         help="path to training data",
-        type=str,
-    )
-    parser.add_argument(
-        "--year",
-        required=True,
-        help="year",
         type=str,
     )
     parser.add_argument(
@@ -692,6 +881,8 @@ if __name__ == "__main__":
     parser.add_argument("--seed", default=42, help="seed for testing/training split", type=int)
     add_bool_arg(parser, "evaluate-only", "Only evaluation, no training", default=False)
     add_bool_arg(parser, "multiclass", "Classify each background separately", default=True)
+    add_bool_arg(parser, "legacy", "Legacy PNet versions", default=False)
+
     args = parser.parse_args()
 
     if args.config_name is None:
