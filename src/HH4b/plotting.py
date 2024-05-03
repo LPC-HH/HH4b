b3b99818bffae8f1046a5b24400031fd942d404f
Cristina <criss.ms7@gmail.com>
2024-05-03 07:28:35 -0500

    postprocess updates

--- src/HH4b/plotting.py
+++ src/HH4b/plotting.py
@@ -640,8 +640,6 @@ def ratioHistPlot(
             )
 
     # plot background errors
-    print(bg_keys)
-    print("I am here")
     if bg_err is None:
         # get background error from variances
         bg_tot = sum([hists[sample, :] for sample in bg_keys])
