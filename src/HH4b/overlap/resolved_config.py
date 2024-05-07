d490ed6cc89105b73f2686481c963834e8bff861
Daniel Primosch <daniel.primosch@cern.ch>
2024-05-06 23:52:24 -0700

    added jet0, jet1 mods. Still missing MET

--- src/HH4b/overlap/resolved_config.py
+++ src/HH4b/overlap/resolved_config.py
@@ -11,24 +11,26 @@ def bdt_dataframe_resolved(events):
     """
 
     # Assuming the first two b-tagged jets are H1 and the next two are H2
+    jet0 = events.query("subentry == 0")
+    jet1 = events.query("subentry == 0")
+
     h1 = vector.array(
         {
-            "pt": events["ak8_pt"][:, 0:2].sum(axis=1),
-            "phi": events["ak8_phi"][:, 0:2].mean(
-                axis=1
-            ),  # Approximate phi of the composite particle, check if valid
-            "eta": events["ak8_eta"][:, 0:2].mean(axis=1),
-            "M": events["dHH_H1_regmass"],
+            "pt": jet0["ak8_pt"],
+            "phi": jet0["ak8_phi"],
+            "eta": jet0["ak8_eta"],
+            "M": jet0["dHH_H1_regmass"],
         }
     )
     h2 = vector.array(
         {
-            "pt": events["ak8_pt"][:, 2:4].sum(axis=1),
-            "phi": events["ak8_phi"][:, 2:4].mean(axis=1),
-            "eta": events["ak8_eta"][:, 2:4].mean(axis=1),
-            "M": events["dHH_H2_regmass"],
+            "pt": jet1["ak8_pt"],
+            "phi": jet1["ak8_phi"],
+            "eta": jet1["ak8_eta"],
+            "M": jet1["dHH_H2_regmass"],
         }
     )
+
     hh = h1 + h2
 
     df_events = pd.DataFrame(
@@ -38,10 +40,10 @@ def bdt_dataframe_resolved(events):
             "HHeta": hh.eta,
             "HHmass": hh.mass,
             # met in the event
-            "MET": events["MET_pt"],  # Check how MET is stored and adjust accordingly
+            "MET": events["MET_pt"],  # TODO: Check how MET is stored and adjust accordingly
             # pseudo-fatjet tau32
-            "H1T32": events["ak8_tau3"][:, 0] / events["ak8_tau2"][:, 0],
-            "H2T32": events["ak8_tau3"][:, 1] / events["ak8_tau2"][:, 1],
+            "H1T32": jet0["ak8_tau3"] / jet0["ak8_tau2"],
+            "H2T32": jet1["ak8_tau3"] / jet1["ak8_tau2"],
             # pseudo-fatjet mass
             "H1Mass": h1.M,
             # pseudo-fatjet kinematics
@@ -50,10 +52,10 @@ def bdt_dataframe_resolved(events):
             "H1eta": h1.eta,
             "H2eta": h2.eta,
             # xbb
-            "H1Xbb": events["ak8_Txbb"].to_numpy()[:, 0],
-            "H1QCD1HF": events["ak8_PQCDb"].to_numpy()[:, 0],
-            "H1QCD2HF": events["ak8_PQCDbb"].to_numpy()[:, 0],
-            "H1QCD0HF": events["ak8_PQCDothers"].to_numpy()[:, 0],
+            "H1Xbb": jet0["ak8_Txbb"].to_numpy(),
+            "H1QCD1HF": jet0["ak8_PQCDb"].to_numpy(),
+            "H1QCD2HF": jet0["ak8_PQCDbb"].to_numpy(),
+            "H1QCD0HF": jet0["ak8_PQCDothers"].to_numpy(),
             # ratios
             "H1Pt_HHmass": h1.pt / hh.mass,
             "H2Pt_HHmass": h2.pt / hh.mass,
