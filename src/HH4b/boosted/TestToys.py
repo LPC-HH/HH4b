3a2aad4b160d641ffaca586a8b258b38eef4520c
Cristina <criss.ms7@gmail.com>
2024-05-09 15:24:11 -0500

    add toys w templates

--- src/HH4b/boosted/TestToys.py
+++ src/HH4b/boosted/TestToys.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import argparse
 import importlib
+from pathlib import Path
 
 import hist
 import matplotlib.pyplot as plt
@@ -9,9 +10,11 @@ import matplotlib.ticker as mticker
 import mplhep as hep
 import numpy as np
 import xgboost as xgb
+import pandas as pd
 
-from HH4b import utils
-from HH4b.postprocessing import load_columns_legacy
+from HH4b import utils, postprocessing
+from HH4b.hh_vars import samples_run3, bg_keys
+from HH4b.utils import ShapeVar
 
 formatter = mticker.ScalarFormatter(useMathText=True)
 formatter.set_powerlimits((-3, 3))
@@ -26,6 +29,7 @@ bdt_axis = hist.axis.Regular(60, 0, 1, name="bdt")
 diff_axis = hist.axis.Regular(100, -2, 2, name="diff")
 cut_axis = hist.axis.StrCategory([], name="cut", growth=True)
 
+legacy_label = "Legacy"
 
 def get_toy_from_hist(h_hist):
     """
@@ -51,13 +55,6 @@ def get_dataframe(events_dict, year, bdt_model_name, bdt_config):
         f".{bdt_config}", package="HH4b.boosted.bdt_trainings_run3"
     )
 
-    HLTs = {
-        "2022EE": [
-            "AK8PFJet250_SoftDropMass40_PFAK8ParticleNetBB0p35",
-        ],
-    }
-
-    legacy_label = "Legacy"
     bdt_events_dict = {}
     for key in events_dict:
         events = events_dict[key]
@@ -72,10 +69,11 @@ def get_dataframe(events_dict, year, bdt_model_name, bdt_config):
         bdt_events["H1TXbb"] = events[f"bbFatJetPNetTXbb{legacy_label}"][0]
         bdt_events["H2TXbb"] = events[f"bbFatJetPNetTXbb{legacy_label}"][1]
         bdt_events["weight"] = events["finalWeight"].to_numpy()
+        
         bdt_events["hlt"] = np.any(
-            np.array([events[trigger][0] for trigger in HLTs[year] if trigger in events]),
+            np.array([events[trigger][0] for trigger in postprocessing.HLTs[year] if trigger in events]),
             axis=0,
-        )
+        )        
         mask_hlt = bdt_events["hlt"] == 1
 
         # masks
@@ -169,41 +167,43 @@ def abcd_fom(
 
 def main(args):
 
-    # FIXME: Load samples for all years..
-    samples_run3 = {
-        "2022EE": {
-            "data": ["JetMET_Run"],
-            "hh4b": ["GluGlutoHHto4B_kl-1p00_kt-1p00_c2-0p00_TuneCP5_13p6TeV"],
-        },
-    }
+    # do not load any QCD samples
+    for year in samples_run3:
+        for key in ["qcd"]:
+            if key in samples_run3[year]:
+                samples_run3[year].pop(key)
+    
     data_dir = "24Apr23LegacyLowerThresholds_v12_private_signal"
     input_dir = f"/eos/uscms/store/user/cmantill/bbbb/skimmer/{data_dir}"
     year = "2022EE"
 
-    events_dict = utils.load_samples(
-        input_dir,
-        samples_run3[year],
-        year,
-        filters=None,
-        columns=utils.format_columns(
-            load_columns_legacy + [("AK8PFJet250_SoftDropMass40_PFAK8ParticleNetBB0p35", 1)]
-        ),
+    events_dict = postprocessing.load_run3_samples(
+        input_dir=input_dir,
+        year=year,
+        legacy=True,
+        samples_run3=samples_run3,
         reorder_txbb=True,
-        txbb="bbFatJetPNetTXbbLegacy",
-        variations=False,
+        txbb=f"bbFatJetPNetTXbb{legacy_label}",
     )
+
     mass_var = "H2PNetMass"
     bdt_events_dict = get_dataframe(events_dict, year, args.bdt_model_name, args.bdt_config)
-    ntoys = 200
 
+    # get other backgrouds
+    bdt_events_dict["others"] = pd.concat([bdt_events_dict[key] for key in bg_keys if key!="qcd"])
+
+    ntoys = args.ntoys
+    print(f"Number of toys {ntoys}")
+    
     mass_window = [110, 140]
-    # bdt_cuts = [0.8, 0.85, 0.9, 0.95]
+
     all_bdt_cuts = 0.01 * np.arange(80, 100)
     bdt_cuts = all_bdt_cuts
-    # xbb_cuts = [0.8]
-    xbb_cuts = [0.8, 0.95]
 
-    bdt_fail = 0.6
+    xbb_cuts = [0.8] #, 0.95]
+
+    # define fail region for ABCD
+    bdt_fail = 0.03
 
     h_pull = hist.Hist(diff_axis, cut_axis)
 
@@ -211,16 +211,37 @@ def main(args):
     kfactor_signal = 80
     print(f"Fixed factor by which to scale signal: {kfactor_signal}")
 
+    # variable to fit
+    fit_shape_var = ShapeVar(
+        mass_var,
+        r"$m^{2}_\mathrm{reg}$ (GeV)",
+        [16, 60, 220],
+        reg=True,
+        # blind or not?
+        blind_window=None
+        # blind_window=window_by_mass[args.mass],
+    )
+    
     expected_by_xbb = {}
     for xbb_cut in xbb_cuts:
+        templ_dir = Path(f"templates/toys_{args.tag}")
+        (templ_dir / year).mkdir(parents=True, exist_ok=True)
+        (templ_dir / "cutflows" / year).mkdir(parents=True, exist_ok=True)
+        
         print(f"\n Xbb cut:{xbb_cut}")
         bdt_events_data = bdt_events_dict["data"][bdt_events_dict["data"]["H2TXbb"] > xbb_cut]
         bdt_events_sig = bdt_events_dict["hh4b"][bdt_events_dict["hh4b"]["H2TXbb"] > xbb_cut]
+        bdt_events_others = bdt_events_dict["others"][bdt_events_dict["others"]["H2TXbb"] > xbb_cut]
         bdt_events_data_invertedXbb = bdt_events_dict["data"][
             bdt_events_dict["data"]["H2TXbb"] < 0.8
         ]
+        bdt_events_others_invertedXbb = bdt_events_dict["others"][
+            bdt_events_dict["others"]["H2TXbb"] < 0.8
+        ]
 
-        # get expected sensitivity for each bdt cut
+        ################################################
+        # Evaluate expected sensitivity for each BDT cut
+        ################################################
         expected_soverb_by_bdt_cut = {}
         cuts = []
         figure_of_merits = []
@@ -239,25 +260,24 @@ def main(args):
                 nevents_sig, nevents_bkg = abcd_fom(
                     mass_data=bdt_events_data[mass_var],
                     mass_sig=bdt_events_sig[mass_var],
-                    mass_others=None,  # no contribution from other backgrounds
+                    mass_others=bdt_events_others[mass_var],
                     cut_data=(bdt_events_data["bdt_score"] >= bdt_cut),
                     cut_sig=(bdt_events_sig["bdt_score"] >= bdt_cut),
-                    cut_others=None,
+                    cut_others=(bdt_events_others["bdt_score"] >= bdt_cut),
                     weight_data=bdt_events_data["weight"],
                     weight_signal=bdt_events_sig["weight"],
-                    weight_others=None,
+                    weight_others=bdt_events_others["weight"],
                     # INVERTED stuff
                     mass_inv_data=bdt_events_data_invertedXbb[mass_var],
-                    mass_inv_others=None,
+                    mass_inv_others=bdt_events_others_invertedXbb[mass_var],
                     invcut_data=(
                         bdt_events_data_invertedXbb["bdt_score"] < bdt_fail
                     ),  # fail region
-                    invcut_others=None,
+                    invcut_others=(bdt_events_others_invertedXbb["bdt_score"] < bdt_fail),
                     weight_inv_data=bdt_events_data_invertedXbb["weight"],
-                    weight_inv_others=None,
+                    weight_inv_others=bdt_events_others_invertedXbb["weight"],
                     mass_window=mass_window,
                 )
-
             nevents_sig_scaled = nevents_sig * kfactor_signal
             soversb = nevents_sig_scaled / np.sqrt(nevents_bkg + nevents_sig_scaled)
             expected_soverb_by_bdt_cut[bdt_cut] = soversb
@@ -280,6 +300,10 @@ def main(args):
 
         print(f"Expected sensitivity by BDT cut {expected_soverb_by_bdt_cut}")
 
+        ###################
+        # TOYS
+        ###################
+        
         # create toy from data mass distribution (with xbb cut)
         h_mass = hist.Hist(mass_axis)
         h_mass.fill(bdt_events_data[mass_var])
@@ -301,10 +325,6 @@ def main(args):
 
             # same thing but for mass distribution with inverted Xbb cut
             random_mass_invertedXbb = get_toy_from_hist(h_mass_invertedXbb)
-            # do not concatenate toys
-            mass_toy_invertedXbb = np.concatenate([random_mass_invertedXbb])
-            bdt_toy_invertedXbb = np.concatenate([bdt_events_data_invertedXbb["bdt_score"]])
-            weight_toy_invertedXbb = np.concatenate([bdt_events_data_invertedXbb["weight"]])
 
             max_soversb = 0
             cuts = []
@@ -324,23 +344,23 @@ def main(args):
                     nevents_sig_bdt_cut, nevents_bkg_bdt_cut = abcd_fom(
                         mass_toy,
                         bdt_events_sig[mass_var],
-                        None,
+                        bdt_events_others[mass_var],
                         (bdt_toy >= bdt_cut),
                         (bdt_events_sig["bdt_score"] >= bdt_cut),
-                        None,
+                        (bdt_events_others["bdt_score"] >= bdt_cut),
                         weight_toy,
                         bdt_events_sig["weight"] * kfactor_signal,
-                        None,
-                        # INVERTED stuff
-                        mass_toy_invertedXbb,
-                        None,
-                        (bdt_toy_invertedXbb < bdt_fail),
-                        None,
-                        weight_toy_invertedXbb,
-                        None,
+                        bdt_events_others["weight"],
+                        # INVERTED stuff (w/o signal)
+                        random_mass_invertedXbb,
+                        bdt_events_others_invertedXbb[mass_var],
+                        (bdt_events_data_invertedXbb["bdt_score"] < bdt_fail),
+                        (bdt_events_others_invertedXbb["bdt_score"] < bdt_fail),
+                        bdt_events_data_invertedXbb["weight"],
+                        bdt_events_others_invertedXbb["weight"],
                         mass_window,
                     )
-
+                    
                 soversb = nevents_sig_bdt_cut / np.sqrt(nevents_bkg_bdt_cut + nevents_sig_bdt_cut)
 
                 # NOTE: here optimizing by soversb but can change the figure of merit...
@@ -353,6 +373,7 @@ def main(args):
                     figure_of_merits.append(soversb)
 
             # choose "optimal" bdt cut, check if it gives the expected sensitivity
+            optimal_bdt_cut = 0
             if len(cuts) > 0:
                 cuts = np.array(cuts)
                 figure_of_merits = np.array(figure_of_merits)
@@ -366,6 +387,45 @@ def main(args):
                     cut=str(xbb_cut),
                 )
 
+            # for this toy the BDT and Xbb cut are
+            print("Xbb ",xbb_cut, "BDT ",optimal_bdt_cut)
+
+            # define regions
+            selection_regions = {
+                "pass_bin1": Region(
+                    cuts={
+                        "H2TXbb": [xbb_cut, CUT_MAX_VAL],
+                        "bdt_score": [optimal_bdt_cut, CUT_MAX_VAL],
+                    },
+                    label="Bin1",
+                ),
+                "fail": Region(
+                    cuts={
+                        "H2TXbb": [-CUT_MAX_VAL, xbb_cut],
+                    },
+                    label="Fail",
+                ),
+            }
+            
+            # replace data distribution with toy
+            bdt_events_for_templates = bdt_events_dict.copy()
+            bdt_events_for_templates["data"][mass_var] = mass_toy
+            
+            templates = postprocessing.get_templates(
+                bdt_events_dict,
+                year=year,
+                sig_keys=["hh4b"],
+                selection_regions=selection_regions,
+                shape_vars=[fit_shape_var],
+                systematics={},
+                template_dir=templ_dir,
+                bg_keys=bg_keys,
+                plot_dir=f"{templ_dir}/{year}",
+                weight_key="weight",
+                show=False,
+                energy=13.6,
+            )
+            
     # plot pull
     fig, ax = plt.subplots(1, 1, figsize=(6, 5))
     for xbb_cut in xbb_cuts:
@@ -396,6 +456,18 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--method", required=True, choices=["sideband", "abcd"], help="method to test"
+    )
+    parser.add_argument(
+        "--ntoys",
+        help="number of toys",
+        type=int,
+        default=1,
+    )
+    parser.add_argument(
+        "--tag",
+        help="tag test",
+        required=True,
+        type=str
     )
     args = parser.parse_args()
     main(args)
