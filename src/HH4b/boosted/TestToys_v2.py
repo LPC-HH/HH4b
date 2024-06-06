b63e79bc9159688bb5bd152969735aede23f3898
Cristina Mantilla <cmantill@uaf-2.t2.ucsd.edu>
2024-06-05 18:10:50 -0700

    update test

--- src/HH4b/boosted/TestToys_v2.py
+++ src/HH4b/boosted/TestToys_v2.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import argparse
 import importlib
-from pathlib import Path
 
 import hist
 import matplotlib.pyplot as plt
@@ -13,7 +12,7 @@ import pandas as pd
 import xgboost as xgb
 
 from HH4b import postprocessing
-from HH4b.hh_vars import bg_keys, samples_run3
+from HH4b.hh_vars import bg_keys
 
 formatter = mticker.ScalarFormatter(useMathText=True)
 formatter.set_powerlimits((-3, 3))
@@ -175,19 +174,44 @@ def main(args):
     # data_dir = "24Apr23LegacyLowerThresholds_v12_private_signal"
     data_dir = "24May24_v12_private_signal"
     input_dir = f"/ceph/cms/store/user/cmantill/bbbb/skimmer/{data_dir}"
-    year = "2022EE"
+    years = ["2022"]  # , "2022EE"] #, "2023", "2023BPix"]
 
-    events_dict = postprocessing.load_run3_samples(
-        input_dir=input_dir,
-        year=year,
-        legacy=True,
-        samples_run3=samples_run3,
-        reorder_txbb=True,
-        txbb=f"bbFatJetPNetTXbb{legacy_label}",
-    )
+    samples_run3 = {
+        year: {
+            "data": [f"{key}_Run" for key in ["JetMET"]],
+            "ttbar": ["TTto4Q", "TTto2L2Nu", "TTtoLNu2Q"],
+            "diboson": ["ZZ", "WW", "WZ"],
+            "vjets": ["Wto2Q-3Jets_HT", "Zto2Q-4Jets_HT"],
+            "hh4b": ["GluGlutoHHto4B_kl-1p00_kt-1p00_c2-0p00_TuneCP5_13p6TeV?"],
+        }
+        for year in years
+    }
 
     mass_var = "H2PNetMass"
-    bdt_events_dict = get_dataframe(events_dict, year, args.bdt_model_name, args.bdt_config)
+
+    bdt_events_per_year = {}
+    for year in years:
+        bdt_events_per_year[year] = {}
+        events_dict = postprocessing.load_run3_samples(
+            input_dir=input_dir,
+            year=year,
+            legacy=True,
+            samples_run3=samples_run3,
+            reorder_txbb=True,
+            txbb=f"bbFatJetPNetTXbb{legacy_label}",
+        )
+        bdt_events = get_dataframe(events_dict, year, args.bdt_model_name, args.bdt_config)
+        columns = ["H2TXbb", "bdt_score", mass_var]
+        for key in events_dict:
+            bdt_events_per_year[year][key] = bdt_events[key][columns]
+
+    print(bdt_events_per_year)
+    # concatenate
+    bdt_events_dict, _ = postprocessing.combine_run3_samples(
+        bdt_events_per_year,
+        ["data", "ttbar", "vjets", "diboson", "hh4b"],
+        years_run3=years,
+    )
 
     # get other backgrounds
     bdt_events_dict["others"] = pd.concat([bdt_events_dict[key] for key in bg_keys if key != "qcd"])
@@ -195,8 +219,8 @@ def main(args):
     ntoys = args.ntoys
     print(f"Number of toys {ntoys}")
 
-    mass_window = [110, 140]
-    # mass_window = [105, 150]
+    # mass_window = [110, 140]
+    mass_window = [100, 150]
 
     all_bdt_cuts = 0.01 * np.arange(80, 100)
     bdt_cuts = all_bdt_cuts
@@ -210,14 +234,12 @@ def main(args):
     bdt_fail = 0.03
 
     # fixed signal k-factor
-    kfactor_signal = 80
+    # kfactor_signal = 80
+    # kfactor_signal = 40
+    kfactor_signal = 20
     print(f"Fixed factor by which to scale signal: {kfactor_signal}")
 
     # get "truth" optimization and sensitivity per cut
-    expected_soverb_by_xbb_cut_by_bdt_cut = {}
-    expected_s_by_xbb_cut_by_bdt_cut = {}
-    expected_b_by_xbb_cut_by_bdt_cut = {}
-
     #########################################
     # Evaluate expected sensitivity from data
     #########################################
@@ -226,9 +248,6 @@ def main(args):
     figure_of_merits = []
 
     for xbb_cut in xbb_cuts:
-        expected_soverb_by_xbb_cut_by_bdt_cut[xbb_cut] = {}
-        expected_s_by_xbb_cut_by_bdt_cut[xbb_cut] = {}
-        expected_b_by_xbb_cut_by_bdt_cut[xbb_cut] = {}
 
         bdt_events_dict_xbb_cut = {}
         for key in bdt_events_dict:
@@ -236,7 +255,7 @@ def main(args):
                 bdt_events_dict[key]["H2TXbb"] > xbb_cut
             ]
 
-        print(f"\n Xbb cut:{xbb_cut}")
+        # print(f"\n Xbb cut:{xbb_cut}")
         bdt_events_data = bdt_events_dict_xbb_cut["data"]
         bdt_events_sig = bdt_events_dict_xbb_cut["hh4b"]
         bdt_events_others = bdt_events_dict_xbb_cut["others"]
@@ -269,17 +288,19 @@ def main(args):
             )
             soversb = nevents_sig / np.sqrt(nevents_bkg + nevents_sig)
 
-            expected_soverb_by_xbb_cut_by_bdt_cut[xbb_cut][bdt_cut] = soversb
-            expected_s_by_xbb_cut_by_bdt_cut[xbb_cut][bdt_cut] = nevents_sig
-            expected_b_by_xbb_cut_by_bdt_cut[xbb_cut][bdt_cut] = nevents_bkg
+            nevents_sideband = np.sum(
+                (
+                    (bdt_events_data[mass_var] < mass_window[0])
+                    | (bdt_events_data[mass_var] > mass_window[1])
+                )
+                & (bdt_events_data["bdt_score"] >= bdt_cut)
+            )
 
-            if nevents_sig > 0.5 and nevents_bkg >= 2:
+            print("nevents_sideband ", nevents_sideband)
+            if nevents_sig > 0.5 and nevents_bkg >= 2 and nevents_sideband >= 12:
                 bdt_cuts_data.append(bdt_cut)
                 xbb_cuts_data.append(xbb_cut)
                 figure_of_merits.append(soversb)
-
-        print(f"Expected sensitivity by BDT cut {expected_soverb_by_xbb_cut_by_bdt_cut[xbb_cut]}")
-        # print(f"Expected S by BDT cut {expected_s_by_xbb_cut_by_bdt_cut[xbb_cut]}")
 
     sensitivity_data = 0
     if len(bdt_cuts_data) > 0:
@@ -293,7 +314,7 @@ def main(args):
         sensitivity_data = figure_of_merits[biggest]
 
         print(
-            f"From Data: Xbb:{optimal_xbb_cut_data:.3f} BDT:{optimal_bdt_cut_data:.2f} S/(S+B):{sensitivity_data:.2f} S: {expected_s_by_xbb_cut_by_bdt_cut[optimal_xbb_cut_data][optimal_bdt_cut_data]:.2f}"
+            f"From Data: Xbb:{optimal_xbb_cut_data:.3f} BDT:{optimal_bdt_cut_data:.2f} S/(S+B):{sensitivity_data:.2f}"
         )
 
     ###################
@@ -322,11 +343,19 @@ def main(args):
     bdt_events_sig = bdt_events_dict["hh4b"]
     bdt_events_others = bdt_events_dict["others"]
 
+    print("nevents data ", bdt_events_data[mass_var].shape[0])
+    print("nevents sig ", np.sum(bdt_events_sig["weight"] * kfactor_signal))
+
+    h_mass_sig = hist.Hist(mass_axis)
+    h_mass_sig.fill(bdt_events_sig[mass_var])
+    h_xbb_sig = hist.Hist(xbb_axis)
+    h_xbb_sig.fill(bdt_events_sig["H2TXbb"])
+
     print("Xbb BDT Index-BDT S/(S+B) Difference Expected")
     for itoy in range(ntoys):
-        templ_dir = Path(f"templates/toys_{args.tag}/xbb_cut_{xbb_cut:.3f}/toy_{itoy}")
-        (templ_dir / year).mkdir(parents=True, exist_ok=True)
-        (templ_dir / "cutflows" / year).mkdir(parents=True, exist_ok=True)
+        # templ_dir = Path(f"templates/toys_{args.tag}/xbb_cut_{xbb_cut:.3f}/toy_{itoy}")
+        # (templ_dir / year).mkdir(parents=True, exist_ok=True)
+        # (templ_dir / "cutflows" / year).mkdir(parents=True, exist_ok=True)
 
         random_xbb = get_toy_from_hist(h_xbb)
         random_mass = get_toy_from_hist(h_mass)
@@ -353,7 +382,17 @@ def main(args):
         bdt_cuts_toys = []
         xbb_cuts_toys = []
         figure_of_merit_toys = []
+        figure_of_merit_abcd_toys = []
+
         signal_toys = []
+        background_toys = []
+        truesignal_toys = []
+        truebackground_toys = []
+
+        sbleft_toys = []
+        sbright_toys = []
+
+        signal_abcd_toys = []
 
         for xbb_cut in xbb_cuts:
             for bdt_cut in bdt_cuts:
@@ -363,6 +402,59 @@ def main(args):
                 nevents_toy_bdt_cut = np.sum(
                     weight_toy[(cut_mass_toy) & (bdt_toy >= bdt_cut) & (xbb_toy >= xbb_cut)]
                 )
+
+                # true number of signal events
+                cut_mass_sig = (bdt_events_sig[mass_var] >= mass_window[0]) & (
+                    bdt_events_sig[mass_var] <= mass_window[1]
+                )
+                nevents_sig_true = np.sum(
+                    (bdt_events_sig["weight"] * kfactor_signal)[
+                        (bdt_events_sig["bdt_score"] >= bdt_cut)
+                        & (bdt_events_sig["H2TXbb"] >= xbb_cut)
+                        & cut_mass_sig
+                    ]
+                )
+
+                # true number of bkg events
+                cut_mass_data = (random_mass >= mass_window[0]) & (random_mass <= mass_window[1])
+                nevents_bkg_true = np.sum(
+                    bdt_events_data["weight"][
+                        (random_bdt >= bdt_cut) & (random_xbb >= xbb_cut) & cut_mass_data
+                    ]
+                )
+
+                # signal events in sideband
+                nevents_sig_left = np.sum(
+                    (bdt_events_sig["weight"] * kfactor_signal)[
+                        (bdt_events_sig["bdt_score"] >= bdt_cut)
+                        & (bdt_events_sig["H2TXbb"] >= xbb_cut)
+                        & (bdt_events_sig[mass_var] < mass_window[0])
+                    ]
+                )
+                nevents_sig_right = np.sum(
+                    (bdt_events_sig["weight"] * kfactor_signal)[
+                        (bdt_events_sig["bdt_score"] >= bdt_cut)
+                        & (bdt_events_sig["H2TXbb"] >= xbb_cut)
+                        & (bdt_events_sig[mass_var] > mass_window[1])
+                    ]
+                )
+                nevents_data_left = np.sum(
+                    [
+                        (random_bdt >= bdt_cut)
+                        & (random_xbb >= xbb_cut)
+                        & (random_mass < mass_window[0])
+                    ]
+                )
+                nevents_data_right = np.sum(
+                    [
+                        (random_bdt >= bdt_cut)
+                        & (random_xbb >= xbb_cut)
+                        & (random_mass > mass_window[1])
+                    ]
+                )
+                # print(xbb_cut,bdt_cut)
+                # print("nsig left right ",nevents_sig_left,nevents_sig_right)
+                # print("ndata left right ",nevents_data_left, nevents_data_right)
 
                 nevents_sig_bdt_cut_sb, nevents_bkg_bdt_cut_sb = sideband_fom(
                     mass_data=mass_toy,
@@ -403,6 +495,8 @@ def main(args):
                 # S_mc
                 # s_from_mc = nevents_sig_bdt_cut
                 # S_toy
+                # abcd already takes into account others
+                # s_from_toy = nevents_toy_bdt_cut - nevents_bkg_bdt_cut - nevents_others_bdt_cut
                 s_from_toy = nevents_toy_bdt_cut - nevents_bkg_bdt_cut
 
                 # sig = s_from_mc
@@ -410,6 +504,7 @@ def main(args):
 
                 soversb = sig / np.sqrt(sig + b_from_toy)
                 # print(xbb_cut, bdt_cut, soversb, " sfromtoy ", s_from_toy, " sfrommc ",s_from_mc)
+                soversb_abcd = soversb
 
                 if args.method == "sideband":
                     # print("abcd ",soversb)
@@ -419,12 +514,33 @@ def main(args):
                     sig = nevents_toy_bdt_cut - nevents_sig_bdt_cut_sb
                     # print("sideband ", soversb)
 
+                nevents_sideband = np.sum(
+                    weight_toy[
+                        ((mass_toy < mass_window[0]) | (mass_toy > mass_window[1]))
+                        & (bdt_toy >= bdt_cut)
+                        & (xbb_toy >= xbb_cut)
+                    ]
+                )
+                # print("n sideband ",nevents_sideband)
+
                 # NOTE: here optimizing by soversb but can change the figure of merit...
-                if nevents_sig_bdt_cut > 0.5 and nevents_bkg_bdt_cut >= 2:
+                if (
+                    nevents_sig_bdt_cut > 0.5 and nevents_bkg_bdt_cut >= 2
+                ):  # and nevents_sideband >= 12:
                     bdt_cuts_toys.append(bdt_cut)
                     xbb_cuts_toys.append(xbb_cut)
+
                     figure_of_merit_toys.append(soversb)
+                    figure_of_merit_abcd_toys.append(soversb_abcd)
+
                     signal_toys.append(sig)
+                    background_toys.append(b_from_toy)
+                    truebackground_toys.append(nevents_bkg_true)
+                    truesignal_toys.append(nevents_sig_true)
+
+                    sbleft_toys.append(nevents_sig_left / nevents_data_left)
+                    sbright_toys.append(nevents_sig_right / nevents_data_right)
+                    signal_abcd_toys.append(nevents_toy_bdt_cut - nevents_bkg_bdt_cut)
 
         # choose "optimal" bdt and xbb cut, check if it gives the expected sensitivity
         optimal_bdt_cut = 0
@@ -434,20 +550,42 @@ def main(args):
             xbb_cuts_toys = np.array(xbb_cuts_toys)
             figure_of_merit_toys = np.array(figure_of_merit_toys)
             biggest = np.argmax(figure_of_merit_toys)
+            truesignal_toys = np.array(truesignal_toys)
 
             optimal_bdt_cut = bdt_cuts_toys[biggest]
             optimal_xbb_cut = xbb_cuts_toys[biggest]
 
-            compare_to = expected_soverb_by_xbb_cut_by_bdt_cut[optimal_xbb_cut][optimal_bdt_cut]
-            compare_to_s = expected_s_by_xbb_cut_by_bdt_cut[optimal_xbb_cut][optimal_bdt_cut]
+            # compare_to = expected_soverb_by_xbb_cut_by_bdt_cut[optimal_xbb_cut][optimal_bdt_cut]
+            # compare_to_s = expected_s_by_xbb_cut_by_bdt_cut[optimal_xbb_cut][optimal_bdt_cut]
+            compare_to_s = truesignal_toys[biggest]
+            # compare_to = truesignal_toys[biggest]/np.sqrt(truesignal_toys[biggest]+background_toys[biggest])
+            compare_to = truesignal_toys[biggest] / np.sqrt(
+                truesignal_toys[biggest] + truebackground_toys[biggest]
+            )
+
             print(
-                f"Optimal {optimal_xbb_cut:.3f} {optimal_bdt_cut:.2f}, FOM: {biggest} {figure_of_merit_toys[biggest]:.2f}, Difference: {(figure_of_merit_toys[biggest]-compare_to):.2f}, Expected S/S+B: {compare_to:.2f}, Expected S: {compare_to_s:.2f}"
+                f" Optimal {optimal_xbb_cut:.3f} {optimal_bdt_cut:.2f}, FOM: {figure_of_merit_toys[biggest]:.2f} \n"
+                + f" S from-Method: {signal_toys[biggest]:.3f}, S from-ABCD: {signal_abcd_toys[biggest]:.3f}, S True: {truesignal_toys[biggest]:.3f} \n"
+                + f" B from-Method: {background_toys[biggest]:.2f}, B True: {truebackground_toys[biggest]:.3f} \n"
+                # +f" S/B left: {sbleft_toys[biggest]} right sideband: {sbright_toys[biggest]}"
+                # Difference: {(figure_of_merit_toys[biggest]-compare_to):.2f},
+                + f" S/S+B: {figure_of_merit_abcd_toys[biggest]:.2f} VS {compare_to:.2f} \n"
+                + f" S: {signal_abcd_toys[biggest]:.2f} VS {compare_to_s:.2f}"
             )
 
             pull = (figure_of_merit_toys[biggest] - compare_to) / compare_to
-            pull_s = (signal_toys[biggest] - compare_to_s) / compare_to_s
             diff = figure_of_merit_toys[biggest] - compare_to
-            diff_s = signal_toys[biggest] - compare_to_s
+
+            # pull_s = (signal_toys[biggest] - truesignal_toys[biggest]) / truesignal_toys[biggest]
+            # diff_s = signal_toys[biggest] - truesignal_toys[biggest]
+
+            pull_s = (signal_abcd_toys[biggest] - truesignal_toys[biggest]) / truesignal_toys[
+                biggest
+            ]
+            diff_s = signal_abcd_toys[biggest] - truesignal_toys[biggest]
+
+            # print("t ",truesignal_toys[biggest])
+            # print(f" Signal-From-Toy: {signal_toys[biggest]}, Signa-From-MC: {truesignal_toys[biggest]}")
 
             pull_array.append(pull)
             pull_s_array.append(pull_s)
@@ -455,12 +593,13 @@ def main(args):
             diff_s_array.append(diff_s)
 
             h_pull.fill(pull)
-            h_pull_s.fill(pull_s)
             h_diff.fill(diff)
+
+            h_pull_s.fill(pull_s)
             h_diff_s.fill(diff_s)
 
             # for this toy the BDT and Xbb cut are
-            print("Optimal cuts: Xbb ", optimal_xbb_cut, "BDT ", optimal_bdt_cut)
+            print(f"{itoy} Optimal cuts: Xbb ", optimal_xbb_cut, "BDT ", optimal_bdt_cut)
 
     # plot diff
     pull_array = np.array(pull_array)
@@ -476,15 +615,16 @@ def main(args):
     }
 
     def plot_h(h_hist, xlabel, plot_name, xlim, gaus_label):
-        optimal = expected_soverb_by_xbb_cut_by_bdt_cut[optimal_xbb_cut_data][optimal_bdt_cut]
+        # optimal = expected_soverb_by_xbb_cut_by_bdt_cut[optimal_xbb_cut_data][optimal_bdt_cut]
 
         fig, ax = plt.subplots(1, 1, figsize=(14, 8))
         mu, sigma = gaus_fit[gaus_label]
         hep.histplot(
             h_hist,
             ax=ax,
-            label=r"Expected S/$\sqrt{S+B}$ from optimal cut in data: "
-            + f" {optimal:.1f}, Mean: {mu:.2f}",
+            label=f"Mean: {mu:.2f}",
+            # "#r"Expected S/$\sqrt{S+B}$ from optimal cut in data: "
+            # + f" {optimal:.1f}, Mean: {mu:.2f}",
         )
         ax.set_xlabel(xlabel)
         plot_title = r"Injected S, S $\times$ " + f"{kfactor_signal}, 2022EE"
@@ -504,7 +644,7 @@ def main(args):
     )
     plot_h(
         h_diff_s,
-        r"S$_{t}$ - S",
+        r"S$_{t}$ - S (ABCD)",
         "s_diff",
         [-2, 2],
         "diff_s",
