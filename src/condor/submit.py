a5b6e53a66d1caa3b08fe6a0c0b7cac0f4991a2c
Daniel Primosch <daniel.primosch@cern.ch>
2024-09-06 15:01:44 -0700

    PNetPlots wip

--- src/condor/submit.py
+++ src/condor/submit.py
@@ -33,7 +33,8 @@ def write_template(templ_file: str, out_file: str, templ_args: dict):
 
 def main(args):
     # check that branch exists
-    run_utils.check_branch(args.git_branch, args.allow_diff_local_repo)
+    run_utils.check_branch(args.git_branch, args.git_user, args.allow_diff_local_repo)
+    username = os.environ["USER"]
 
     if args.site == "lpc":
         try:
@@ -42,18 +43,19 @@ def main(args):
             print("No valid proxy. Exiting.")
             exit(1)
     elif args.site == "ucsd":
-        proxy = "/home/users/rkansal/x509up_u31735"
+        if username == "rkansal":
+            proxy = "/home/users/rkansal/x509up_u31735"
+        elif username == "dprimosc":
+            proxy = "/tmp/x509up_u150012"  # "/home/users/dprimosc/x509up_u150012"
     else:
         raise ValueError(f"Invalid site {args.site}")
 
     if args.site not in args.save_sites:
         warnings.warn(
-            f"Your local site {args.site} is not in save sites {args.sites}!", stacklevel=1
+            f"Your local sit e {args.site} is not in save sites {args.sites}!", stacklevel=1
         )
 
     t2_prefixes = [t2_redirectors[site] for site in args.save_sites]
-
-    username = os.environ["USER"]
 
     tag = f"{args.tag}_{args.nano_version}_{args.region}"
 
