45e06eeb6f1c50f445d819e9a46e9f6316d33ffb
Daniel Primosch <daniel.primosch@cern.ch>
2024-09-12 19:15:04 -0700

    plotting ROC for PNET wip

--- src/HH4b/plotting.py
+++ src/HH4b/plotting.py
@@ -1315,7 +1315,7 @@ def ROCCurve(
     plt.legend()
 
     if len(name):
-        plt.savefig(plot_dir / f"{name}.pdf", bbox_inches="tight")
+        plt.savefig(f"{plot_dir}/{name}.pdf", bbox_inches="tight")
 
     if show:
         plt.show()
