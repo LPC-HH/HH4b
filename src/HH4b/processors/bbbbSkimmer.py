e297e463aaab098f72bbb005f64c3a60acfe5494
Cristina <criss.ms7@gmail.com>
2024-09-27 17:05:31 -0500

    temp fix to submit w/o trigger selection

--- src/HH4b/processors/bbbbSkimmer.py
+++ src/HH4b/processors/bbbbSkimmer.py
@@ -760,7 +760,7 @@ class bbbbSkimmer(SkimmerABC):
         )
 
         # apply trigger
-        apply_trigger = True
+        apply_trigger = False
         if (~is_run3) and (~isData) and self._region == "signal":
             # in run2 we do not apply the trigger to MC
             apply_trigger = False
