b67f36142d74eb76ee6d5127c06ca22f8635922f
Cristina <criss.ms7@gmail.com>
2024-09-29 08:09:26 -0500

    add logging

---
+++ src/HH4b/log_utils.py
@@ -0,0 +1,19 @@
+## https://fangpenlin.com/posts/2012/08/26/good-logging-practice-in-python/
+log_config = {
+    "version": 1,
+    "disable_existing_loggers": False,  # this fixes the problem about many loggers
+    "formatters": {
+        "default": {"format": "[%(asctime)s] %(levelname)-2s: %(name)-20s %(message)s"},
+    },
+    "handlers": {
+        "default": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "default",
+        },
+    },
+    "root": {
+        "level": "INFO",
+        "handlers": ["default"],
+    },
+}
