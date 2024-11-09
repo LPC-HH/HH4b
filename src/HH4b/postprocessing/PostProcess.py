d3dd3f93a7f4e95662730a651a373e5dfab91f10
Javier Duarte <jduarte@ucsd.edu>
2024-11-08 20:53:15 -0800

    update mass window

--- src/HH4b/postprocessing/PostProcess.py
+++ src/HH4b/postprocessing/PostProcess.py
@@ -1281,8 +1281,8 @@ def postprocess_run3(args):
         blind_window_by_mass["H2PNetMass"] = [110, 140]  # only blind 3 bins
     # different for glopart-v2
     elif args.txbb == "glopart-v2":
-        fom_window_by_mass["H2PNetMass"] = [115, 160]  # use wider range for FoM scan
-        blind_window_by_mass["H2PNetMass"] = [120, 150]  # only blind 3 bins
+        fom_window_by_mass["H2PNetMass"] = [110, 155]  # use wider range for FoM scan
+        blind_window_by_mass["H2PNetMass"] = [110, 140]  # only blind 3 bins
     # different for pnet-v12
     elif args.txbb == "pnet-v12":
         fom_window_by_mass["H2PNetMass"] = [120, 150]
