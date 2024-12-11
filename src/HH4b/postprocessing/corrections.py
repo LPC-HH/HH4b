6fb6a7c9dd64419ed1b95b20f0d27fc5927a2fc7
Zichun Hao <zhao@caltech.edu>
2024-12-11 15:10:26 -0800

    Extract bins automatically

--- src/HH4b/postprocessing/corrections.py
+++ src/HH4b/postprocessing/corrections.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+import warnings
 from pathlib import Path
 
 import correctionlib
@@ -158,195 +159,71 @@ def ttbar_SF(
     return sfs["nominal"], sfs["stat_up"], sfs["stat_dn"]
 
 
+def _get_json_fname(year: str, label: str, region: str):
+    return f"{package_path}/corrections/data/fatjet_triggereff_{year}_{label}_{region}.json"
+
+
 def _load_trig_effs(year: str, label: str, region: str):
-    return correctionlib.CorrectionSet.from_file(
-        f"{package_path}/corrections/data/fatjet_triggereff_{year}_{label}_{region}.json"
-    )
+    fname = _get_json_fname(year, label, region)
+    return correctionlib.CorrectionSet.from_file(fname)
+
+
+def _get_bins(year: str, label: str, region: str) -> dict[str, list[float]]:
+    """Extract bins from json file"""
+    fname = _get_json_fname(year, label, region)
+    json_dict = json.load(Path.open(fname))
+    sample_dict = json_dict["corrections"][0]["data"]["content"][0]["value"]
+
+    edge_dict = {}
+    if "input" in sample_dict:
+        # only one variable
+        key = sample_dict["input"]
+        value = sample_dict["edges"]
+        edge_dict[key] = value
+    elif "inputs" in sample_dict:
+        # multiple variables
+        keys = sample_dict["inputs"]
+        values = sample_dict["edges"]
+        for key, value in zip(keys, values):
+            edge_dict[key] = value
+    else:
+        raise ValueError(
+            f"json file {fname} has no 'input' or 'inputs'. Found: {sample_dict.keys()}"
+        )
+
+    return edge_dict
 
 
 def trigger_SF(year: str, events_dict: dict[str, pd.DataFrame], txbb_str: str, region: str):
     """
     Evaluate trigger Scale Factors
     """
-
-    # hard code axes
-    if "2023" in year:
-        pt_range = [
-            0.0,
-            10.0,
-            20.0,
-            30.0,
-            40.0,
-            50.0,
-            60.0,
-            70.0,
-            80.0,
-            90.0,
-            100.0,
-            110.0,
-            120.0,
-            130.0,
-            140.0,
-            150.0,
-            160.0,
-            170.0,
-            180.0,
-            190.0,
-            200.0,
-            210.0,
-            220.0,
-            230.0,
-            240.0,
-            250.0,
-            260.0,
-            270.0,
-            280.0,
-            290.0,
-            300.0,
-            320.0,
-            340.0,
-            360.0,
-            380.0,
-            400.0,
-            420.0,
-            440.0,
-            460.0,
-            480.0,
-            500.0,
-            550.0,
-            600.0,
-            700.0,
-            800.0,
-            1000.0,
-        ]
-        xbb_range = [
-            0.0,
-            0.05,
-            0.1,
-            0.15,
-            0.2,
-            0.25,
-            0.3,
-            0.35,
-            0.4,
-            0.45,
-            0.5,
-            0.55,
-            0.6,
-            0.65,
-            0.7,
-            0.75,
-            0.8,
-            0.85,
-            0.9,
-            0.95,
-            1.0,
-        ]
+    txbb = "txbb" if ("Legacy" in txbb_str or "ParT" in txbb_str) else "txbbv11"
+    if "Legacy" in txbb_str:
+        txbb_str = "txbbPNet"
+    elif "ParT" in txbb_str:
+        txbb_str = "txbbGloParT"
     else:
-        pt_range = [
-            0.0,
-            30.0,
-            60.0,
-            90.0,
-            120.0,
-            150.0,
-            180.0,
-            210.0,
-            240.0,
-            270.0,
-            300.0,
-            360.0,
-            420.0,
-            480.0,
-            600.0,
-            1000.0,
-        ]
-        xbb_range = [
-            0.0,
-            0.04,
-            0.08,
-            0.12,
-            0.16,
-            0.2,
-            0.24,
-            0.28,
-            0.32,
-            0.36,
-            0.4,
-            0.44,
-            0.48,
-            0.52,
-            0.56,
-            0.6,
-            0.64,
-            0.68,
-            0.72,
-            0.76,
-            0.8,
-            0.84,
-            0.88,
-            0.92,
-            0.96,
-            1.0,
-        ]
-    xbbv11_range = [
-        0.0,
-        0.05,
-        0.1,
-        0.15,
-        0.2,
-        0.25,
-        0.3,
-        0.35,
-        0.4,
-        0.45,
-        0.5,
-        0.55,
-        0.6,
-        0.65,
-        0.7,
-        0.75,
-        0.8,
-        0.85,
-        0.9,
-        0.95,
-        1.0,
-    ]
-    msd_range = [
-        0.0,
-        5.0,
-        10.0,
-        20.0,
-        30.0,
-        40.0,
-        50.0,
-        60.0,
-        80.0,
-        100.0,
-        120.0,
-        150.0,
-        200.0,
-        250.0,
-        300.0,
-        350.0,
-    ]
+        warnings.warn(f"txbb_str {txbb_str} not recognized. Using it as is.", stacklevel=2)
 
-    # for now, assume same trigger SFs for PNet Legacy and GloParTv2
-    if "Legacy" in txbb_str or "ParT" in txbb_str:
-        txbb = "txbb"
-        xbb_axis = hist.axis.Variable(xbb_range, name="xbb")
-    else:
-        txbb = "txbbv11"
-        xbb_axis = hist.axis.Variable(xbbv11_range, name="xbb")
-    pt_axis = hist.axis.Variable(pt_range, name="pt")
-    msd_axis = hist.axis.Variable(msd_range, name="msd")
     # load trigger efficiencies
     triggereff_ptmsd = _load_trig_effs(year, "ptmsd", region)
-    triggereff_btag = _load_trig_effs(year, txbb, region)
+    triggereff_btag = _load_trig_effs(year, txbb_str, region)
+
     eff_data = triggereff_ptmsd[f"fatjet_triggereffdata_{year}_ptmsd"]
     eff_mc = triggereff_ptmsd[f"fatjet_triggereffmc_{year}_ptmsd"]
-    eff_data_btag = triggereff_btag[f"fatjet_triggereffdata_{year}_{txbb}"]
-    eff_mc_btag = triggereff_btag[f"fatjet_triggereffmc_{year}_{txbb}"]
+    eff_data_btag = triggereff_btag[f"fatjet_triggereffdata_{year}_{txbb_str}"]
+    eff_mc_btag = triggereff_btag[f"fatjet_triggereffmc_{year}_{txbb_str}"]
+
+    # extract bins
+    ptmsd_bins_dict = _get_bins(year, "ptmsd", region)
+    pt_range = ptmsd_bins_dict["pt"]
+    msd_range = ptmsd_bins_dict["msd"]
+    xbb_range = _get_bins(year, txbb_str, region)["xbb"]
+
+    pt_axis = hist.axis.Variable(pt_range, name="pt")
+    msd_axis = hist.axis.Variable(msd_range, name="msd")
+    xbb_axis = hist.axis.Variable(xbb_range, name="xbb")
 
     # efficiencies per jet
     eff_data_per_jet = {}
@@ -363,7 +240,7 @@ def trigger_SF(year: str, events_dict: dict[str, pd.DataFrame], txbb_str: str, r
     for jet in range(2):
         pt = events_dict["bbFatJetPt"][jet]
         msd = events_dict["bbFatJetMsd"][jet]
-        xbb = events_dict[txbb_str][jet]
+        xbb = events_dict[txbb][jet]
 
         num_ev = pt.shape[0]
 
