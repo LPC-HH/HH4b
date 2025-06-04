af239b68ba3c84ceeb372e58087e2b5befa9151d
Zichun Hao <zichunh2000@gmail.com>
2025-06-04 17:09:37 -0500

    Add nano version "v12_ZbbSFZMuMu"

--- src/HH4b/run_utils.py
+++ src/HH4b/run_utils.py
@@ -193,6 +193,7 @@ def parse_common_args(parser):
             "v12",
             "v12_private",
             "v12v2_private",
+            "v12_ZbbSFZMuMu",
         ],
         help="NanoAOD version",
     )
