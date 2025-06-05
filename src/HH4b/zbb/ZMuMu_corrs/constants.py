444b4c616c147d37b31665802e758d4a391b1829
Zichun Hao <zichunh2000@gmail.com>
2025-06-05 00:19:23 -0700

    Consider VV samples, apply POG muon corrections to muon 2, and use all years in ZQQ->DYLL

---
+++ src/HH4b/zbb/ZMuMu_corrs/constants.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import numpy as np
+
+MASS_RANGE = (80, 100)
+PT_RANGE = (300, np.inf)
+
+PT_BINS = np.array([300, 400, 550, 700, 1000, 2000])
+MASS_BINS = np.arange(*MASS_RANGE, 1)
+
+# https://pdg.lbl.gov/2025/tables/rpp2025-sum-gauge-higgs-bosons.pdf
+BR_Z_QQ = 0.69911  # hadrons
+BR_Z_EE = 0.033632
+BR_Z_MUMU = 0.033662
+BR_Z_TAUTAU = 0.033696
