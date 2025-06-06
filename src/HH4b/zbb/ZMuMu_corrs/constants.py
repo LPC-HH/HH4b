2e6b95a45921bd70faa27e26157db05000dec7b4
Zichun Hao <zichunh2000@gmail.com>
2025-06-06 14:17:21 -0700

    Apply the correct MuonPOG corrections, use shape corrections for ZQQ->DYLL, and update pT bins

--- src/HH4b/zbb/ZMuMu_corrs/constants.py
+++ src/HH4b/zbb/ZMuMu_corrs/constants.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 import numpy as np
 
 MASS_RANGE = (80, 100)
-PT_RANGE = (300, np.inf)
+PT_RANGE = (200, np.inf)
 
-PT_BINS = np.array([300, 400, 550, 700, 1000, 2000])
+PT_BINS = np.array([200, 250, 300, 350, 400, 450, 500, 550, 600, 700, 800, 1000, 2000])
 MASS_BINS = np.arange(*MASS_RANGE, 1)
 
 # https://pdg.lbl.gov/2025/tables/rpp2025-sum-gauge-higgs-bosons.pdf
