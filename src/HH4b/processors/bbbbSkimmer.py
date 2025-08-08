6f067da1ff43322d1e3d1079497a945e5aafa3b7
Zichun Hao <zichunh2000@gmail.com>
2025-08-08 14:43:20 -0500

    Still use JMR=1 as the nominal value

--- src/HH4b/processors/bbbbSkimmer.py
+++ src/HH4b/processors/bbbbSkimmer.py
@@ -511,11 +511,10 @@ class bbbbSkimmer(SkimmerABC):
                     "down": 1.0 - d_jms,
                     "up": 1.0 + d_jms,
                 }
-                # JMR is set to be >= 1.0
                 jmr_val = {
-                    "nom": 1.0 + d_jmr,
-                    "down": 1.0,
-                    "up": 1.0 + 2 * d_jmr,
+                    "nom": 1.0,
+                    "down": 1.0 - d_jmr,
+                    "up": 1.0 + d_jmr,
                 }
 
             self.jmr_values[jmsr_year] = dict.fromkeys(self.jmsr_vars)
